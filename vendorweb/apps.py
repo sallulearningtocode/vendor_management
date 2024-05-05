1f6d85d52260cccda8015f9536a406a80a1dfe19
Salman Khan <khansarlman66498@gmail.com>
2024-05-05 08:49:07 +0530

    second

---
+++ vendorweb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VendorwebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vendorweb'
---
+++ vendorweb/serializers.py
@@ -0,0 +1,6 @@
+# from rest_framework import serializers
+# class StudentSerializer(serializers.Serializer):
+#     name=serializers.charField(max_length=100)
+#     roll = serializers.IntegerField()
+#     city = serializers.IntegerField(max_length=100)
+    
